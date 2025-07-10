f0a7c6e63a2a74032262904ebcc8e4645f53ad6f
AgustinValetti <agustinvaletti@gmail.com>
2025-07-10 00:23:54 +0000

    Tp Final

--- RTA_Examen_20250706/Punto_D.sh
+++ RTA_Examen_20250706/Punto_D.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-ansible-playbook -i ~/UTN-FRA_SO_Examenes/202411/ansible/inventory/hosts ~/UTN-FRA_SO_Examenes/202411/ansible/playbook.yml
+mkdir -p ~/RTA_Examen_20250705/ansible/roles/2PRecuperatorio/tasks
+mkdir -p ~/RTA_Examen_20250705/ansible/roles/Crea_Carpetas_Valetti/tasks
+mkdir -p ~/RTA_Examen_20250705/ansible/roles/Cambia_Propiedad_Valetti/tasks
+mkdir -p ~/RTA_Examen_20250705/ansible/roles/Sudoers_Valetti/tasks
+
+cp ~/UTN-FRA_SO_Examenes/202411/ansible/roles/2PRecuperatorio/tasks/main.yml ~/RTA_Examen_20250705/ansible/roles/2PRecuperatorio/tasks/
+cp ~/UTN-FRA_SO_Examenes/202411/ansible/roles/Crea_Carpetas_Valetti/tasks/main.yml ~/RTA_Examen_20250705/ansible/roles/Crea_Carpetas_Valetti/tasks/
+cp ~/UTN-FRA_SO_Examenes/202411/ansible/roles/Cambia_Propiedad_Valetti/tasks/main.yml ~/RTA_Examen_20250705/ansible/roles/Cambia_Propiedad_Valetti/tasks/
+cp ~/UTN-FRA_SO_Examenes/202411/ansible/roles/Sudoers_Valetti/tasks/main.yml ~/RTA_Examen_20250705/ansible/roles/Sudoers_Valetti/tasks/
+cp ~/UTN-FRA_SO_Examenes/202411/ansible/playbook.yml ~/RTA_Examen_20250705/ansible/
+
+echo "Completado"
+
